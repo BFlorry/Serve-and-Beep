71c019fc0d72c4ab5c0c2f1f38b0c44bcb0bbab1
vailhou <huugolio@gmail.com>
2020-03-16 22:54:05 +0200

    Update AIController.cs

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -10,6 +10,22 @@ public class AIController : MonoBehaviour
 {
     //Fields------------------------------------------------------------------------
 
+    //An area where an NPC can move from another area.
+    [SerializeField]
+    private GameObject area1;
+
+    //An area where an NPC can move from another area.
+    [SerializeField]
+    private GameObject area2;
+
+    //An area where an NPC can move from another area.
+    [SerializeField]
+    private GameObject area3;
+
+    private List<GameObject> areas;
+
+    private int areaId = -1;
+
     private IEnumerator waitAfterMove;
 
     [SerializeField]
@@ -27,6 +43,15 @@ public class AIController : MonoBehaviour
 
     //Methods------------------------------------------------------------------------
 
+    private void Start()
+    {
+        areas.Add(new GameObject());
+        areas.Add(area1);
+        areas.Add(area2);
+        areas.Add(area3);
+    }
+
+
     void Update()
     {
         MoveRandomly();
@@ -36,6 +61,8 @@ public class AIController : MonoBehaviour
     /// <summary>
     /// If moving and if close enough to target, set waiting.
     /// If not moving and not waiting, move to new location.
+    /// If AIController has valid area id set, move inside that area.
+    /// Otherwise move randomly inside whole NavMesh.
     /// </summary>
     private void MoveRandomly()
     {
@@ -53,7 +80,17 @@ public class AIController : MonoBehaviour
 
         else if (waitAfterMove == null)
         {
-            Move(RandomPoint(Vector3.zero, walkRadius)); //could be (transform.position, walkRadius)
+            //If AIController has valid area id, get random point inside area.
+            if (0 <= areaId)
+            {
+                GameObject area = areas[areaId];
+                Move(RandomPointInObject(area));
+            }
+            //If AIController does not have valid area id, get random point inside whole NavMesh.
+            else
+            {
+                Move(RandomPoint(Vector3.zero, walkRadius)); //could be (transform.position, walkRadius)
+            }
             moving = true;
         }
         
@@ -100,4 +137,33 @@ public class AIController : MonoBehaviour
 
         return hit.position;
     }
+
+
+    /// <summary>
+    /// Sets an id of an area where the player will move to.
+    /// </summary>
+    /// <param name="areaId">Id of an area where the player will move to.</param>
+    public void MoveToArea(int areaId)
+    {
+        this.areaId = areaId;
+    }
+
+
+    /// <summary>
+    /// Returns random point on NavMesh within given object.
+    /// </summary>
+    /// <param name="area">The GameObject in which the returned point will be.</param>
+    /// <returns>A random point on NavMesh within given object.</returns>
+    public Vector3 RandomPointInObject(GameObject area)
+    {
+        Bounds bounds = area.GetComponent<Bounds>();
+        Vector3 point = new Vector3(
+            Random.Range(bounds.min.x, bounds.max.x),
+            0,
+            Random.Range(bounds.min.z, bounds.max.z));
+        NavMeshHit hit;
+        NavMesh.SamplePosition(point, out hit, float.MaxValue, 1);
+        return hit.position;
+    }
+
 }

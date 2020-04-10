e004b39aa6ca64c7a87e2a6d1c88fc7558e5482e
Mikko Mäntylä <mijumant@student.jyu.fi>
2020-04-10 19:57:08 +0300

    Added more file extensions to file-extension editor extension and added lastmodified-value to it

--- Assets/3rdparty/EditorExtensions/FileExtensionForProjectPanel/Editor/FileExtensionGUI.cs
+++ Assets/3rdparty/EditorExtensions/FileExtensionForProjectPanel/Editor/FileExtensionGUI.cs
@@ -25,6 +25,11 @@ public class FileExtensionGUI
         ".ogg",
         ".mp3",
         ".wav",
+        ".cs",
+        ".mat",
+        ".prefab",
+        ".shader",
+        ".pdf"
     };
 
     static FileExtensionGUI()
@@ -47,19 +52,33 @@ public class FileExtensionGUI
     {
         string path = AssetDatabase.GUIDToAssetPath(guid);
         string extRaw = Path.GetExtension(path);
-        if (!showExt.Contains(extRaw))
+        if (!showExt.Contains(extRaw.ToLower()))
             return;
 
         bool selected = false;
         if (ValidString(guid) && ValidString(selectedGuid))
             selected = String.Compare(guid, 0, selectedGuid, 0, 6) == 0;
 
+        DateTime lastModification = File.GetLastWriteTime(path);
+
+        TimeSpan span = DateTime.Now.Subtract(lastModification);
+        string fileElapsedText = string.Format("{0}{1}{2}{3}",
+            span.Duration().Days > 0 ? string.Format("{0:0} day{1}, ", span.Days, span.Days == 1 ? string.Empty : "s") : string.Empty,
+            span.Duration().Hours > 0 ? string.Format("{0:0} hour{1}, ", span.Hours, span.Hours == 1 ? string.Empty : "s") : string.Empty,
+            span.Duration().Minutes > 0 ? string.Format("{0:0} min{1}, ", span.Minutes, span.Minutes == 1 ? string.Empty : "s") : string.Empty,
+            span.Duration().Seconds > 0 ? string.Format("{0:0} sec{1}", span.Seconds, span.Seconds == 1 ? string.Empty : "s") : string.Empty);
+
+        if (fileElapsedText.EndsWith(", ")) fileElapsedText = fileElapsedText.Substring(0, fileElapsedText.Length - 2);
+
+        if (string.IsNullOrEmpty(fileElapsedText)) fileElapsedText = "0 sec";
 
         sb.Clear().Append(extRaw);
         if (sb.Length > 0)
         {
             sb.Remove(0, 1);
         }
+
+        sb.Append(" | " + fileElapsedText);
 
         string ext = sb.ToString();
 
@@ -72,11 +91,15 @@ public class FileExtensionGUI
         var size = style.CalcSize(new GUIContent(ext));
         //EditorGUI.DrawRect(selectionRect, new Color(.76f, .76f, .76f));
         //selectionRect.x -= size.x + 12;
-        selectionRect.x += size.x + 200;
+        //selectionRect.x += size.x + 10;
+
+        selectionRect.x += Path.GetFileName(path).Length * 8;
 
 
         Rect offsetRect = new Rect(selectionRect.position, selectionRect.size);
         EditorGUI.LabelField(offsetRect, ext, style);
+        
 
     }
+
 }

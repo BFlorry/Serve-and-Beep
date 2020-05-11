618ede1cd6a4a01d032c85ad7d643fd68392309b
eloneonn <eloneonn@users.noreply.github.com>
2020-05-11 18:35:13 +0300

    New sfx. Set sfx to pizza prefabs. Volume sliders to logarithmic. Throw charging sfx.

---
+++ Assets/SfxOnStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SfxOnStart : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip startSfx;
+
+    private SoundManager soundManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundManager = FindObjectOfType<SoundManager>();
+        soundManager.PlaySingle(startSfx);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
